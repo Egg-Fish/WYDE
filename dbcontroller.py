73d01d244f467578612e09d65d710df331f94399
paperthing <dalbertchea@gmail.com>
2021-07-23 14:48:00 +0800

    test2

--- dbcontroller.py
+++ dbcontroller.py
@@ -1,5 +1,16 @@
 def add_flashcard(question, answer):
-    pass
+    path = "\db\"
+    f = open("cards.csv","a+")
+    lines = f.readlines()
+    f.close()
+    del lines[1]
+
+    f = open("cards.csv", "w+")
+
+    for line in lines:
+        f.write(line)
+    
+    f.close()
 
 def add_quizquestion(question, answer):
     pass
@@ -15,3 +26,5 @@ def modify_flashcard(id, question, answer):
 
 def modify_quizquestion(id, question, answer):
     pass
+
+#test
