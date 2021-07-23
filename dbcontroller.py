8fd7228b2a2acbdca13d169836a707347cacd90e
Egg-Fish <49740285+Egg-Fish@users.noreply.github.com>
2021-07-23 14:32:51 +0800

    feat: added prototypes

--- dbcontroller.py
+++ dbcontroller.py
@@ -1,1 +1,17 @@
-print("i am wen qi")
+def add_flashcard(question, answer):
+    pass
+
+def add_quizquestion(question, answer):
+    pass
+
+def remove_flashcard(id):
+    pass
+
+def remove_quizquestion(id):
+    pass
+
+def modify_flashcard(id, question, answer):
+    pass
+
+def modify_quizquestion(id, question, answer):
+    pass
